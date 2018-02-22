736f9fc8dfd6812e80dd2d44a71aeb709d762d4e
jneilliii <jneilliii+github@gmail.com>
2018-02-21 23:48:38 -0500

    test

--- octoprint_M117SpeechSynthesis/static/js/M117SpeechSynthesis.js
+++ octoprint_M117SpeechSynthesis/static/js/M117SpeechSynthesis.js
@@ -2,78 +2,92 @@ $(function() {
     function M117SpeechSynthesisViewModel(parameters) {
         var self = this;
 		
-	self.settingsViewModel = parameters[0];
-		
-	self.enableSpeech = ko.observable();
-	self.speechVoice = ko.observable();
-	self.speechVolume = ko.observable();
-	self.speechPitch = ko.observable();
-	self.speechRate = ko.observable();
-	self.speechLanguage = ko.observable();
-	self.voices = ko.observableArray();
-	self.speechEnabledBrowser = ko.observable();
-
-	self.onDataUpdaterPluginMessage = function(plugin, data) {
-            	if (plugin != "M117SpeechSynthesis") {
-                	return;
-            	}
+		self.settingsViewModel = parameters[0];
 			
-		if(data.type == "speak") {
-			if(self.enableSpeech() && ('speechSynthesis' in window)){
-				var msg = new SpeechSynthesisUtterance(data.msg);
-				msg.volume = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume();
-				msg.pitch = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch();
-				msg.rate = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate();
-				msg.lang = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage();
-				msg.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVoice(); })[0];
-				speechSynthesis.cancel();
-				speechSynthesis.speak(msg);
+		self.enableSpeech = ko.observable();
+		self.speechVoice = ko.observable();
+		self.speechVolume = ko.observable();
+		self.speechPitch = ko.observable();
+		self.speechRate = ko.observable();
+		self.speechLanguage = ko.observable();
+		self.voices = ko.observableArray();
+		self.speechEnabledBrowser = ko.observable();
+
+		self.onDataUpdaterPluginMessage = function(plugin, data) {
+			if (plugin != "M117SpeechSynthesis") {
+				return;
+			}
+				
+			if(data.type == "speak") {
+				if(self.enableSpeech() && ('speechSynthesis' in window)){
+					var msg = new SpeechSynthesisUtterance(data.msg);
+					msg.volume = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume();
+					msg.pitch = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch();
+					msg.rate = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate();
+					msg.lang = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage();
+					msg.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == self.speechVoice(); })[0];
+					speechSynthesis.cancel();
+					speechSynthesis.speak(msg);
+				}
 			}
 		}
-	}
-		
-	self.onBeforeBinding = function() {
-		self.enableSpeech(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.enableSpeech());
-		self.speechVoice(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVoice());
-		self.speechVolume(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume());
-		self.speechPitch(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch());
-		self.speechRate(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate());
-		self.speechLanguage(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage());
-		if('speechSynthesis' in window) {
-			self.speechEnabledBrowser(true);
-			self.loadVoices();
-		} else { 
-			self.speechEnabledBrowser(false);
-		}
-        }
-		
-	self.onEventSettingsUpdated = function (payload) {
-		self.enableSpeech(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.enableSpeech());
-		self.speechVoice(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVoice());
-		self.speechVolume(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume());
-		self.speechPitch(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch());
-		self.speechRate(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate());
-		self.speechLanguage(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage());
-        }
-		
-	self.testVoice = function(data) {
-		self.onDataUpdaterPluginMessage("M117SpeechSynthesis", {'msg':'M117 Speech Synthesis example.','type':'speak'});
-	}
-	
-	self.loadVoices = function() {
-		if (self.voices().length > 0)
-			return;
-		var voicenames = speechSynthesis.getVoices();
-		voicenames.forEach(function(voice, i) {
-			self.voices.push({'name':voice.name,'value':voice.name})
-			});
-	}
 			
-	if ('speechSynthesis' in window) {
-		speechSynthesis.onvoiceschanged = function(e) {
-			self.loadVoices();
-		};
-	}
+		self.onBeforeBinding = function() {
+			self.enableSpeech(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.enableSpeech());
+			self.speechVoice(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVoice());
+			self.speechVolume(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume());
+			self.speechPitch(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch());
+			self.speechRate(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate());
+			self.speechLanguage(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage());
+			if('speechSynthesis' in window) {
+				self.speechEnabledBrowser(true);
+				self.loadVoices();
+			} else { 
+				self.speechEnabledBrowser(false);
+			}
+		}
+			
+		self.onEventSettingsUpdated = function (payload) {
+			self.enableSpeech(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.enableSpeech());
+			self.speechVoice(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVoice());
+			self.speechVolume(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume());
+			self.speechPitch(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch());
+			self.speechRate(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate());
+			self.speechLanguage(self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage());
+		}
+			
+		self.onEventSettingsShown = function (payload) {
+				self.loadVoices();
+		}
+			
+		self.testVoice = function(data) {
+			console.log(data);
+			// if(self.enableSpeech() && ('speechSynthesis' in window)){
+				// var msg = new SpeechSynthesisUtterance("M117 Speech Synthesis example.");
+				// msg.volume = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechVolume();
+				// msg.pitch = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechPitch();
+				// msg.rate = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechRate();
+				// msg.lang = self.settingsViewModel.settings.plugins.M117SpeechSynthesis.speechLanguage();
+				// msg.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == self.speechVoice(); })[0];
+				// speechSynthesis.cancel();
+				// speechSynthesis.speak(msg);
+			// }
+		}
+		
+		self.loadVoices = function() {
+			if (self.voices().length > 0)
+				return;
+			var voicenames = speechSynthesis.getVoices();
+			voicenames.forEach(function(voice, i) {
+				self.voices.push({'name':voice.name,'value':voice.name})
+				});
+			}
+				
+		if ('speechSynthesis' in window) {
+			speechSynthesis.onvoiceschanged = function(e) {
+				self.loadVoices();
+			};
+		}
     }
 
     // This is how our plugin registers itself with the application, by adding some configuration
